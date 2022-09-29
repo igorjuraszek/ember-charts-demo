8ba73c77f176ad04b60137dc55b2161eb305233f
igorjuraszek <igorjuraszek@gmail.com>
2022-09-29 10:19:04 +0200

    adding charts

--- app/pods/cars/controller.js
+++ app/pods/cars/controller.js
@@ -1,3 +1,35 @@
 import Controller from '@ember/controller';
 
-export default class CarsController extends Controller {}
+export default class CarsController extends Controller {
+  get data() {
+    return {
+      datasets: [
+        {
+          data: [10, 20, 30],
+          backgroundColor: [
+            'rgb(255, 0, 0)',
+            'rgb(255, 255, 0)',
+            'rgb(0, 0, 255)',
+          ],
+        },
+      ],
+
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: ['Red', 'Yellow', 'Blue'],
+    };
+  }
+
+  get dataCars() {
+    return {
+      datasets: [
+        {
+          data: [22, 9],
+          backgroundColor: ['rgb(0, 0, 255)', 'rgb(255, 0, 0)'],
+        },
+      ],
+
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: ['Available days', 'Unavailable days'],
+    };
+  }
+}
